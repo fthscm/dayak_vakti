f54a6c527427a92d11a01f9939826f9d9d76be38
fthscm <ganisicim@gmail.com>
2026-05-05 15:53:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const messages = {
     9: "salak de",
     6: "mal de",
     3: "geri zekalı de",
-    1: "ellerine sağlık sarışın bombam"
+    1: "ellerine sağlık Miroşummm"
 };
 
 /* silah seçimi */
